6d6b33321e205277896fd98d851a51cddc4d6e76
nehalist <github@nehalist.io>
2018-11-03 18:07:26 +0100

    Add initial metadata for test

--- packages/Metadata/test/MetadataTest.ts
+++ packages/Metadata/test/MetadataTest.ts
@@ -4,10 +4,12 @@ import {Metadata} from "..";
 
 describe('MetadataTest', () => {
   class Foo {}
+  Reflect.defineMetadata('foobar', 42, Foo);
 
   describe('#get(), #set()', () => {
     it('should set and get metadata', () => {
       Metadata.set('foo', 'bar', Foo);
+      expect(Metadata.get('foobar', Foo)).to.equal(42);
       expect(Metadata.get('foo', Foo)).to.equal('bar');
     });
 
@@ -18,7 +20,7 @@ describe('MetadataTest', () => {
 
   describe('#has()', () => {
     it('should tell if metadata exists', () => {
-      expect(Metadata.has('foo', Foo)).to.be.true;
+      expect(Metadata.has('foobar', Foo)).to.be.true;
       expect(Metadata.has('bar', Foo)).to.be.false;
     });
   });
