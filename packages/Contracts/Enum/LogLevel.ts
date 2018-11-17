856279e187f1a7c3d9fcb7f57aeea2ac1020317e
nehalist <github@nehalist.io>
2018-11-17 23:15:12 +0100

    Fix format

--- packages/Contracts/Enum/LogLevel.ts
+++ packages/Contracts/Enum/LogLevel.ts
@@ -1,10 +1,10 @@
 export enum LogLevel {
   EMERGENCY = 600,
-  ALERT = 550,
-  CRITICAL = 500,
-  ERROR = 400,
-  WARNING = 300,
-  NOTICE = 250,
-  INFO = 200,
-  DEBUG = 100,
+  ALERT     = 550,
+  CRITICAL  = 500,
+  ERROR     = 400,
+  WARNING   = 300,
+  NOTICE    = 250,
+  INFO      = 200,
+  DEBUG     = 100,
 }
