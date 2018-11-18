a928f504c27ac25217a313411d49647ec2ea5391
nehalist <github@nehalist.io>
2018-11-18 17:49:37 +0100

    Add custom format test

--- packages/Logger/test/Formatter/LineFormatterTest.ts
+++ packages/Logger/test/Formatter/LineFormatterTest.ts
@@ -4,9 +4,14 @@ import {ExampleRecord} from "../Fixtures/ExampleRecord";
 
 describe('LineFormatterTest', () => {
   describe('#format()', () => {
-    it('should properly format a simple line', () => {
+    it('should properly format a simple line with default format', () => {
       const formatter = new LineFormatter();
       expect(formatter.format(ExampleRecord)).to.equal(`[${new Date().toLocaleDateString()}] test.ERROR: Foobar`);
+    });
+
+    it('should properly format a simple line with custom format', () => {
+      const formatter = new LineFormatter("%datetime% - %channel%.%level%: %message%");
+      expect(formatter.format(ExampleRecord)).to.equal(`${new Date().toLocaleDateString()} - test.ERROR: Foobar`);
     });
   });
 });
