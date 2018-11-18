9939201041208c3db7e9160ad6a30c3329b7406b
nehalist <github@nehalist.io>
2018-11-18 17:47:10 +0100

    Remove unused constructor

--- packages/Logger/src/Handler/ConsoleHandler.ts
+++ packages/Logger/src/Handler/ConsoleHandler.ts
@@ -2,10 +2,6 @@ import {Record} from "../Record";
 import {AbstractFormattableHandler} from "./AbstractFormattableHandler";
 
 export class ConsoleHandler extends AbstractFormattableHandler {
-  public constructor(protected console?: Console) {
-    super();
-  }
-
   public handle(record: Record) {
     const formatter = this.getFormatter();
 
